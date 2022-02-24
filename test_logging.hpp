91cc8c0859930698d4a88271aa65fc86a5b5c74f
Felix Reissmann <felix.rm.153@gmail.com>
2022-02-24 12:20:20 +0100

    Change the way tests are run
    Previously tests needed to be the 'run' method of a class instance.
    Now all static methods of classes can be run as a test:
    
    TestLogging::run("test display name", TestClass::test_method, args, to, test, method);

--- test_logging.hpp
+++ test_logging.hpp
@@ -11,8 +11,11 @@ class TestLogging {
         std::string fail_reason{};
         bool passed;
 
-        test_result() : passed(true){};
-        test_result(std::string reason) : fail_reason(reason), passed(false){};
+        test_result()
+            : passed(true){};
+
+        test_result(std::string reason)
+            : fail_reason(reason), passed(false){};
     } test_result_t;
 
     typedef struct suite {
@@ -30,7 +33,7 @@ class TestLogging {
         s_suites.back().name = name;
     }
 
-    static void test_start(std::string name) {
+    static void test_start(const std::string& name) {
         s_current_test_name = name;
         printf("\n\n%s::%s \e[33mSTART\e[0m\n", s_suites.back().name.c_str(), s_current_test_name.c_str());
     }
@@ -53,20 +56,41 @@ class TestLogging {
         printf("\n");
     }
 
-    static void results() {
+    template <typename... Args>
+    static void test_printf_start(Args... args) {
+        printf("\t");
+        printf(args...);
+    }
+
+    template <typename... Args>
+    static void test_printf_append(Args... args) {
+        printf(args...);
+    }
+
+    template <typename... Args>
+    static void test_printf_end(Args... args) {
+        printf(args...);
+        printf("\n");
+    }
+
+    static int results() {
+        bool failed = false;
+        ;
         for (auto& suite : s_suites) {
+            if (suite.tests_failed > 0) failed = true;
+
             printf("\n\nsummary for %s: %s (%d/%d passed; %d/%d failed)\n", suite.name.c_str(), suite.tests_failed == 0 ? "\e[32mPASSED\e[0m" : "\e[31mFAILED\e[0m", suite.tests_passed, suite.tests_passed + suite.tests_failed, suite.tests_failed, suite.tests_passed + suite.tests_failed);
             for (auto& fail_reason : suite.fails)
                 printf("\t\e[31m%s::%s failed\e[0m - reason: %s\n", suite.name.c_str(), fail_reason.test_name.c_str(), fail_reason.fail_reason.c_str());
         }
+
+        return failed ? 1 : 0;
     }
 
     template <typename T, typename... Args>
-    static void run(Args... args) {
-        T test_instance{args...};
-
-        test_start(test_instance.test_name);
-        test_result_t res = test_instance.run();
+    static void run(std::string name, T executor, Args... args) {
+        test_start(name);
+        test_result_t res = executor(args...);
 
         if (res.passed) {
             test_pass();
